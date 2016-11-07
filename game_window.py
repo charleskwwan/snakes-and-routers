3ce4513eff84eaa64f5930ce2456505775958686
Charles Wan <yty.charles@gmail.com>
2016-11-06 21:54:15 -0500

    Make snake movable, shift GameWindow to event-based queue timing

--- game_window.py
+++ game_window.py
@@ -1,24 +1,45 @@
 import pygame
 from snake import *
 
+# Lowest possible user event ID (generally 24)
+USEREVENT = pygame.USEREVENT
+CREATE_FOOD = USEREVENT + 1
+MOVE_SNAKE = USEREVENT + 2
+
 class GameWindow(object):
     def __init__(self):
-        pygame.init()
+        pygame.init()         
 
-        self.size = width, height = 500, 500
-        self.screen = pygame.display.set_mode(self.size)
         self.time = pygame.time.Clock()
+        self.fps = 60
+        self.size = self.width, self.height = 500, 500
+        self.screen = pygame.display.set_mode(self.size)
 
+    def runGame(self):
+        # init player's snake and move-snake timer
+        snake = Snake((10, 10), 5, (0, 0, 255), (0, 255, 0), (0, +1))
+        pygame.time.set_timer(MOVE_SNAKE, 200)
+
+        while True:
+            self.time.tick(self.fps)
+            self.screen.fill((255, 255, 255))
+            snake.blit(self.screen)
+            for event in pygame.event.get([pygame.QUIT, MOVE_SNAKE]):
+                if event.type == pygame.QUIT:
+                    exit()
+                elif event.type == MOVE_SNAKE:
+                    snake.update(self.screen)
+            pygame.display.update()
+             
     def run(self):
-        snake = Snake((10, 10), 3, (255, 0, 0), (0, 255, 0), (0, +1))
+        inGame = True
+        inMenu = True
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit()
-            self.screen.fill((255, 255, 255))
-            self.time.tick(10)
-            snake.update(self.screen, [])
-            pygame.display.update()
+            if inGame:
+                self.runGame()
 
 def main():
     app = GameWindow()
--- snake.py
+++ snake.py
@@ -6,118 +6,86 @@
 import pygame
 
 CELL_LEN = 10
-SCREEN_WID = 50 # cells
-SCREEN_HGT = 50
 
-# SnakePart: represents one section of the snake (many sections)
-#	assumed: each cell is 10 by 10 pixels
+# SnakePart: represents one secton of the snake (many sections)
 class SnakePart(object):
-	# cell_x and cell_y is the x and y of the cell
-	# x and y is based on true pixels
-	def __init__(self, cell_pos, color):
-		self.cell_x = cell_pos[0] # set which cell
-		self.cell_y = cell_pos[1]
-		self.x = self.cell_x * CELL_LEN # get true x, y
-		self.y = self.cell_y * CELL_LEN
+	def __init__(self, cell, color):
+		self.col = cell[0]
+		self.row = cell[1]
+		self.x = self.col * CELL_LEN
+		self.y = self.row * CELL_LEN
 		self.color = color
 
-	# function called to draw part on board
+	# function to draw on board
 	def blit(self, screen):
 		part = pygame.Rect(self.x, self.y, CELL_LEN, CELL_LEN)
 		pygame.draw.rect(screen, self.color, part)
 
-# Snake: represents an entire snake
+# Snake represents the entire snake
 class Snake(object):
-	def __init__(self, init_cell_pos, init_len, hd_color, bd_color, init_dir):
-		# positioning details
-		self.cell_x = init_cell_pos[0] # set which cell
-		self.cell_y = init_cell_pos[1]
-		self.x = self.cell_x * CELL_LEN # get true x, y
-		self.y = self.cell_y * CELL_LEN
+	def __init__(self, cell, length, hd_color, bd_color, direction):
+		# positioning
+		self.col = cell[0]
+		self.row = cell[1]
+		self.x = self.col * CELL_LEN
+		self.y = self.row * CELL_LEN
 
 		# head details
 		self.hd_color = hd_color
-		self.head = SnakePart((self.cell_x, self.cell_y), self.hd_color)
+		self.head = SnakePart(cell, hd_color)
 
 		# movement details
-		self.dir_x = init_dir[0]
-		self.dir_y = init_dir[1]
+		self.dir_col = direction[0]
+		self.dir_row = direction[1]
 
 		# body details
-		self.length = init_len # does not include head
-		self.tail = [] # body parts
+		self.length = length
+		self.tail = []
 		self.bd_color = bd_color
-		# create initial snake body, based on initial direction
-		nxt_x = self.head.cell_x + self.dir_x
-		nxt_y = self.head.cell_y + self.dir_y
+		# create snake body
+		nxt_col = self.head.col - self.dir_col
+		nxt_row = self.head.row - self.dir_row
 		for i in range(0, self.length):
-			self.tail.append(SnakePart((nxt_x, nxt_y), bd_color))
-			nxt_x += self.dir_x
-			nxt_y += self.dir_y
+			self.tail.append(SnakePart((nxt_col, nxt_row), bd_color))
+			nxt_col -= self.dir_col
+			nxt_row -= self.dir_row
 
-		#overall details
+		# overall details
 		self.is_dead = False
-		self.vulnerable_in = 10 # once at 0, snake becomes vulnerable
+		self.vulnerable_in = 10 # at 0, snake becomes vulnerable
 
-	def update(self, screen, snakes):
-		self.move()
-		self.blit(screen)
-		# if self.vulnerable_in > 0:
-		# 	self.vulnerable_in -= 1
-		# self.move()
-		# self.blit(screen)
-		# if self.vulnerable_in <= 0:
-		# 	for s in snakes:
-		# 		if self.collideWithSnake(s):
-		# 			self.is_dead = True
-		# 			self.die()
+	def blit(self, screen):
+		for part in self.tail:
+			part.blit(screen)
+		self.head.blit(screen)
 
-	# actions to be completed if the snake dies, respawn
-	def die(self):
-		print "Dead!" # todo: respwan snake at new random positionE
-
-	# checks if self has collided with a given snake
-	#	need to run thru every part of self against every part of other snake
-	#	since invulnerability at beginning
-	def collideWithSnake(self, snake):
-		self_parts = [self.head] + self.tail
-		other_parts = [snake.head] + snake.tail
-		for s in self_parts:
-			for o in other_parts:
-				if s.x == o.x and s.y == o.y:
-					return True
-		return False
-
-	# to be used when eating fruit
-	def increaseLength(self):
-		self.length += 1
-
-	# update position based on user key
-	def move(self, dt):
-
-		key = pygame.key.get_pressed()
-		if key[pygame.K_UP] and self.dir_y != +1:
-			self.dir_x = 0
-			self.dir_y = -1
-		elif key[pygame.K_DOWN] and self.dir_y != -1:
-			self.dir_x = 0
-			self.dir_y = +1
-		elif key[pygame.K_LEFT] and self.dir_x != +1:
-			self.dir_x = -1
-			self.dir_y = 0
-		elif key[pygame.K_RIGHT] and self.dir_x != -1:
-			self.dir_x = +1
-			self.dir_y = 0
-		print "DIR {} {}".format(self.dir_x, self.dir_y)
-		# extend snake
-		self.tail.insert(0, SnakePart((self.cell_x, self.cell_y), self.bd_color)) # add head to tail
-		self.cell_x += self.dir_x # todo: wrap around screen
-		self.cell_x += self.dir_y 
-		self.head = SnakePart((self.cell_x, self.cell_y), self.hd_color) # move head
+	# todo: implement window wrapping on snake movement
+	def move(self):
+		# change direction if necessary
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_UP and self.dir_row != +1:
+					self.dir_col = 0
+					self.dir_row = -1
+				elif event.key == pygame.K_DOWN and self.dir_row != -1:
+					self.dir_col = 0
+					self.dir_row = +1
+				elif event.key == pygame.K_LEFT and self.dir_col != +1:
+					self.dir_col = -1
+					self.dir_row = 0
+				elif event.key == pygame.K_RIGHT and self.dir_col != -1:
+					self.dir_col = +1
+					self.dir_row = 0
+		# extend/move snake based on direction
+		self.tail.insert(0, SnakePart((self.col, self.row), self.bd_color))
+		self.col += self.dir_col
+		self.row += self.dir_row
+		self.x = self.col * CELL_LEN
+		self.y = self.row * CELL_LEN
+		self.head = SnakePart((self.col, self.row), self.hd_color) # head
 		if len(self.tail) > self.length:
 			self.tail.pop(len(self.tail) - 1) # remove last part in tail
 
-	def blit(self, screen):
-		self.head.blit(screen)
-		for p in self.tail:
-			p.blit(screen)
+	def update(self, screen):
+		self.move()
+		self.blit(screen)
