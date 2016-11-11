594444281087970abd5b7a55d9668ee11edb5992
Charles Wan <yty.charles@gmail.com>
2016-11-10 21:25:31 -0500

    Change Player to only create message, not handle networking

--- player.py
+++ player.py
@@ -2,29 +2,16 @@ import PodSixNet
 from PodSixNet.Connection import ConnectionListener, connection
 from PodSixNet.Channel import Channel
 from PodSixNet.Server import Server
-# import GameState
+import GameState
 
-class Player(ConnectionListener, Channel):
+class Player(object):
 	validTypes = ["input", "newSnake", "gameState", "removeSnake"]
 
 	# create a new player, which has its own ipaddr and port identity
 	def __init__(ip, port):
 		self.addr = (ip, port) # used as player's unique id
-		# self.game_state = GameState.GameState()
+		self.game_state = GameState.GameState()
 
-	def sendMessage(dstaddr, ty, msg): # via connection listener
-		self.Connect(dstaddr)
+	def createMessage(ty, msg):
 		data = {"action": ty, "msg": msg} # package as sendable dictionary
 		self.Send(message)
-
-	def Network_input(self, data):
-		print data
-
-	def Network_newSnake(self, data):
-		print data
-
-	def Network_gameState(self, data):
-		print data
-
-	def Network_removeSnake(self, data):
-		print data
