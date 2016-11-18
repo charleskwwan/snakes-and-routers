04c8db193785587f4d119e5fd3cffc2d2332dfc1
Charles Wan <yty.charles@gmail.com>
2016-11-17 20:50:40 -0500

    Combine player and client

--- Player.py
+++ Player.py
@@ -1,33 +1,22 @@
 import PodSixNet
 from PodSixNet.Connection import ConnectionListener, connection
-from PodSixNet.Channel import Channel
-from PodSixNet.Server import Server
 import GameState
+import Messaging
 
-class Player(object):
-	# validTypes = ["input", "joinGame", "newSnake", "gameState", "removeSnake"]
-	validTypes = \
-	INPUT, JOIN_GAME, NEW_SNAKE, GAME_STATE, REMOVE_SNAKE, NEW_FOOD  = \
-	["input", "joinGame", "newSnake", "gameState", "removeSnake", "newFood"]
+class ConnectionException(Exception):
+	pass
 
-	DATA_TAG = "data"
-
+class Player(ConnectionListener):
 	# create a new player, which has its own ipaddr and port identity
-	def __init__(self, ip, port):
-		self.addr = (ip, port) # used as player's unique id
+	def __init__(self):
 		self.game_state = GameState.GameState()
-
-	def createMessage(self, ty, data):
-		if ty in Player.validTypes:
-			# package as sendable dictionary
-			message = {"action": ty, Player.DATA_TAG: data}
-			return message
-		else:
-			raise LookupError("Invalid message type")
+		self.hostaddr = None # created when join game
+		self.addr = None # used as own id, need to query host for it
 
 	def getKey(self):
 		return self.addr
 
+	# game state functions, mainly wrappers
 	def addKeyPressed(self, addr, pressed):
 		self.game_state.addKeyPressed(addr, pressed)
 
@@ -39,3 +28,42 @@ class Player(object):
 
 	def blit(self, screen):
 		self.game_state.blit(screen)
+
+	# networking functions
+	def joinGame(self, hostaddr):
+		self.hostaddr = hostaddr
+		self.Connect(self.hostaddr)
+		message = Messaging.createMessage(Messaging.JOIN_GAME, "")
+		self.Send(message)
+
+	def Network_joinGame(self, message):
+		self.addr = message[Messaging.DATA_TAG] # only thing in message is addr
+
+	def Network_gameState(self, message):
+		encoded_state = message[Messaging.DATA_TAG]
+		self.game_state = GameState(json=encoded_state)
+
+	def sendInput(self, key_pressed):
+		if self.hostaddr == None:
+			raise ConnectionException("Player not connected to host")
+		else:
+			message = Messaging.createMessage(Messaging.INPUT, key_pressed)
+			self.Send(message)
+
+	def Network_input(self, message):
+		data = message[Messaging.DATA_TAG]
+		self.game_state.addKeyPressed(data["addr"], data["key_pressed"])
+
+	def Network_newFood(self, message):
+		food_cell = message[Messaging.DATA_TAG]
+		self.game_state.addFood(food_cell)
+
+	def updateConnection(self):
+		connection.Pump()
+		self.Pump()
+
+if __name__ == "main":
+	player = Player()
+	player,joinGame(("localhost", 9999))
+	while True:
+		player.updateConnection()
